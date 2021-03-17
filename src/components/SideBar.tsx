10e5887f4051be6fb9e0a36575630651175c40dd
cnilton <conexa.caio.barroso@conexasaude.com.br>
2021-03-17 09:40:30 -0300

    challenge finished

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,49 @@
-export function SideBar() {
-  // Complete aqui
+import React, {useState, useEffect} from 'react';
+import { Button } from './Button';
+
+import { api } from '../services/api';
+
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface Props {
+  handleClickButton: (id: number)=> void;
+  selectedGenreId: number;
+}
+
+export function SideBar({handleClickButton, selectedGenreId}: Props) {
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              id={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
